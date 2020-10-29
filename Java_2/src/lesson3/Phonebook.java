74db326ef4306331f3e74e3d3cc01928e3116037
Vitaly1785 <korsar1785@yandex.ru>
2020-10-29 17:15:42 +0300

    Видимо не то спушил

--- Java_2/src/lesson3/Phonebook.java
+++ Java_2/src/lesson3/Phonebook.java
@@ -1,2 +1,26 @@
-package lesson3;public class Phonebook {
+package lesson3;
+
+
+import java.util.*;
+
+public class Phonebook {
+
+
+    Map<String, Set<String>> maps = new LinkedHashMap<>();
+
+    public void addSubscriber(String name, String number) {
+        if(maps.containsKey(name)) {
+            Set<String> numbers = maps.get(name);
+            if (!numbers.contains(number)) {
+                numbers.add(number);
+            }
+        }else{
+            maps.put(name, new HashSet<>(Arrays.asList(number)));
+        }
+    }
+
+    public void getSubscriber(String name) {
+        System.out.println(name + " " + maps.get(name));
+    }
+
 }
